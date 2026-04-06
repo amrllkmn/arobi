401832ca3ea031217abaa3e16dc88f50ee431ebf
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2026-04-06 22:22:44 +0800

    feat: add market order functionality

--- src/order.rs
+++ src/order.rs
@@ -54,6 +54,17 @@ impl Order {
             timestamp,
         }
     }
+
+    pub fn new_market(id: u64, side: Side, quantity: u64, timestamp: u64) -> Self {
+        Self {
+            id,
+            side,
+            order_type: OrderType::Market,
+            price: 0,
+            quantity,
+            timestamp,
+        }
+    }
 }
 
 impl OrderBook {
@@ -183,6 +194,97 @@ impl OrderBook {
 
         fills
     }
+
+    fn add_market_bid_order(&mut self, mut order: Order) -> Vec<Fill> {
+        let mut fills: Vec<Fill> = Vec::new();
+        let mut to_be_deleted: Vec<u64> = Vec::new();
+
+        for (price, price_level) in self.asks.iter_mut() {
+            if order.quantity == 0 {
+                break;
+            } else {
+                while let Some(resting_ask) = price_level.orders.front_mut() {
+                    if order.quantity == 0 {
+                        break;
+                    } else if resting_ask.quantity <= order.quantity {
+                        fills.push(Fill {
+                            price: *price,
+                            quantity: resting_ask.quantity,
+                            maker_order_id: resting_ask.id,
+                            taker_order_id: order.id,
+                        });
+                        order.quantity -= resting_ask.quantity;
+                        self.order_map.remove(&resting_ask.id);
+                        price_level.orders.pop_front();
+
+                        if price_level.orders.is_empty() {
+                            to_be_deleted.push(*price);
+                        }
+                    } else {
+                        fills.push(Fill {
+                            price: *price,
+                            quantity: order.quantity,
+                            maker_order_id: resting_ask.id,
+                            taker_order_id: order.id,
+                        });
+                        resting_ask.quantity -= order.quantity;
+                        break;
+                    }
+                }
+            }
+        }
+        for price in to_be_deleted {
+            self.asks.remove(&price);
+        }
+
+        fills
+    }
+
+    fn add_market_ask_order(&mut self, mut order: Order) -> Vec<Fill> {
+        let mut fills: Vec<Fill> = Vec::new();
+        let mut to_be_deleted: Vec<u64> = Vec::new();
+
+        for (price, price_level) in self.bids.iter_mut().rev() {
+            if order.quantity == 0 {
+                break;
+            } else {
+                while let Some(resting_ask) = price_level.orders.front_mut() {
+                    if order.quantity == 0 {
+                        break;
+                    } else if resting_ask.quantity <= order.quantity {
+                        fills.push(Fill {
+                            price: *price,
+                            quantity: resting_ask.quantity,
+                            maker_order_id: resting_ask.id,
+                            taker_order_id: order.id,
+                        });
+                        order.quantity -= resting_ask.quantity;
+                        self.order_map.remove(&resting_ask.id);
+                        price_level.orders.pop_front();
+
+                        if price_level.orders.is_empty() {
+                            to_be_deleted.push(*price);
+                        }
+                    } else {
+                        fills.push(Fill {
+                            price: *price,
+                            quantity: order.quantity,
+                            maker_order_id: resting_ask.id,
+                            taker_order_id: order.id,
+                        });
+                        resting_ask.quantity -= order.quantity;
+                        break;
+                    }
+                }
+            }
+        }
+        for price in to_be_deleted {
+            self.bids.remove(&price);
+        }
+
+        fills
+    }
+
     pub fn add_limit_order(&mut self, order: Order) -> Vec<Fill> {
         // 1. check if bid or ask
 
@@ -199,8 +301,11 @@ impl OrderBook {
             Side::Ask => self.add_ask_order(order),
         }
     }
-    pub fn add_market_order(&mut self, _side: Side, _qty: u64) -> Vec<Fill> {
-        todo!()
+    pub fn add_market_order(&mut self, order: Order) -> Vec<Fill> {
+        match order.side {
+            Side::Bid => self.add_market_bid_order(order),
+            Side::Ask => self.add_market_ask_order(order),
+        }
     }
     pub fn cancel_order(&mut self, _order_id: u64) -> bool {
         todo!()
